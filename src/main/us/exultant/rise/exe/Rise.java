e130b81af87364b5ee0b1007a9edde43c3d3539e
hash <hash@exultant.us>
2012-09-17 21:29:31 -0700

    Update Rise to be a modern Beard application (get rid of scheduler setup and whatnot stuff that's now already dealt with automatically).

--- src/main/us/exultant/rise/exe/Rise.java
+++ src/main/us/exultant/rise/exe/Rise.java
@@ -5,14 +5,11 @@ import us.exultant.ahs.thread.*;
 import us.exultant.beard.*;
 import us.exultant.rise.view.map.*;
 
-public class Rise implements Beardlet {
+public class Rise extends Beardlet {
 	public void start(final Beard $beard) {
-		$beard.normalizePage();
-		
 		// prove beard works and can touch js at all
 		$beard.eval("$('#main').html('ohai!');");
 		$beard.eval("$('#main').attr('tabindex', 1);");
-		
 		
 		
 		// set up a bunch of demo event listeners that just log really loudly.
@@ -23,10 +20,7 @@ public class Rise implements Beardlet {
 						$beard.console_log($evt.toString());
 					}}
 			);
-		$scheduler = new WorkSchedulerFlexiblePriority(1).start();
-		$scheduler.schedule($beard.bus().getWorkTarget(), ScheduleParams.makeFixedRate(1));
-		
-		
+			
 		
 		// draw a big neato map thingy
 		$beard.eval("$('#main').append("
@@ -37,12 +31,9 @@ public class Rise implements Beardlet {
 		Grider $grider = new Grider(1200,800);
 		$grider.jotAttachImages($beard);
 		$grider.jotElementGenerator($beard, "#rise-map-placer");
-		
 	}
 	
-	private WorkScheduler $scheduler;
-	
 	public void stop() {
-		$scheduler.stop(true);
+		
 	}
 }
