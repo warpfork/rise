74b43514cd28ede4b64fbf3bcc25085c846a9a1c
hash <hash@exultant.us>
2012-09-25 21:43:52 -0700

    Reformed build process to have two different kinds of dist you can trigger separately, one for web and one for standalone.  The standalone product now includes the whole javafx runtime lib, which makes a pretty big file, but it's one file and it just runs.

--- src/main/us/exultant/rise/exe/Rise.java
+++ src/main/us/exultant/rise/exe/Rise.java
@@ -6,6 +6,8 @@ import us.exultant.beard.*;
 import us.exultant.rise.view.map.*;
 
 public class Rise extends Beardlet {
+	public static void main(String... $args) { LaunchStandalone.main(Rise.class.getName()); }
+	
 	public void start(final Beard $beard) {
 		// prove beard works and can touch js at all
 		$beard.eval("$('#main').html('ohai!');");
